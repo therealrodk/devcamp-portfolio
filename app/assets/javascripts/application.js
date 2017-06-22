dc623a4cb8f223bc1083933ae6353a1b24f238f9
Rod <armyofbuddhas@gmail.com>
2017-06-22 01:54:17 -0600

    Integrated cocoon for portfolio items nested form component.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
+//= require cocoon
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-sprockets
